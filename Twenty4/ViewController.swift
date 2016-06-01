5d5525c6fcb18c4daf91c1f08897a649ad5d4213
Jake Esmael <jakeesmael@gmail.com>
2016-05-31 22:20:18 -0700

    Initial Commit

---
+++ Twenty4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Twenty4
+//
+//  Created by Jake Esmael on 5/31/16.
+//  Copyright © 2016 Jake Esmael. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
